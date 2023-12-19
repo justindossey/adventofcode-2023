16ce8e35506c10ba2bd7d6a13d4a4eea9c01e319
Justin Dossey <jbd@justindossey.com>
2023-12-19 09:04:03 -0800

    Add optimization from part 2

--- 18/challenge_part1.lua
+++ 18/challenge_part1.lua
@@ -39,65 +39,24 @@ local function parseInput(input)
 end
 
 -- return the manhattan distance between two points
-local function manhattanDistance(point1, point2)
+local function manhattan_distance(point1, point2)
   return math.abs(point1.x - point2.x) + math.abs(point1.y - point2.y)
 end
 
--- print a grid showing the path
-local function printGrid(intervals)
-  local grid = {}
-  local max_x = 0
-  local min_x = 0
-  local min_y = 0
-  local max_y = 0
-  local out = ""
-  for _, interval in ipairs(intervals) do
-    max_x = math.max(max_x, interval.start.x, interval.finish.x)
-    min_x = math.min(min_x, interval.start.x, interval.finish.x)
-    for x = math.min(interval.start.x, interval.finish.x),
-            math.max(interval.start.x, interval.finish.x) do
-      max_y = math.max(max_y, interval.start.y, interval.finish.y)
-      min_y = math.min(min_y, interval.start.y, interval.finish.y)
-      for y = math.min(interval.start.y, interval.finish.y),
-              math.max(interval.start.y, interval.finish.y) do
-        if not grid[y] then
-          grid[y] = {}
-        end
-        grid[y][x] = "#"
-      end
-    end
+local function calculate_area(vertices)
+  local area = 0
+  local perimeter = 0
+  local j = #vertices
+  for i = 1, #vertices do
+    area = area + (vertices[j].x + vertices[i].x) * (vertices[j].y - vertices[i].y)
+    perimeter = perimeter + manhattan_distance(vertices[j], vertices[i])
+    j = i
   end
-  for y = min_y, #grid do
-    for x = min_x, max_x do
-      if grid[y] and not grid[y][x] then
-        grid[y][x] = "."
-      end
-    end
-  end
-  -- flood fill the 2D grid, replacing all the "." bounded by "#" with "#"
-  local function floodFill(x, y)
-    if grid[y] and grid[y][x] == "." then
-      grid[y][x] = "#"
-      floodFill(x + 1, y)
-      floodFill(x - 1, y)
-      floodFill(x, y + 1)
-      floodFill(x, y - 1)
-    end
-  end
-  floodFill(2, 2) -- arbitrary point inside the walls
-  -- construct a string based on the grid
-  for y = min_y, max_y do
-    for x = min_x, max_x do
-      out = out .. (grid[y][x] or ".")
-    end
-    out = out .. "\n"
-  end
-  return out
+  return math.floor(math.abs(area / 2) + (perimeter / 2) + 1)
 end
 
 local dig_plan = parseInput(readFile(input_file))
-local covered_intervals = {}
-local start_points = {}
+local vertices = {}
 local start_point = {x = 1, y = 1}
 
 -- work through the dig plan and record the covered intervals
@@ -115,21 +74,8 @@ while next(dig_plan) do
   elseif direction == "D" then
     end_point.y = end_point.y + distance
   end
-  table.insert(covered_intervals, {start = start_point, finish = end_point})
-  table.insert(start_points, start_point)
+  table.insert(vertices, start_point)
   start_point = end_point
 end
--- count the total distance covered
-local total_distance = 0
-for _, interval in ipairs(covered_intervals) do
-  total_distance = total_distance + manhattanDistance(interval.start, interval.finish)
-end
--- logd(inspect(start_points))
-local filled_grid = printGrid(covered_intervals)
--- count all the "#" characters in the filled grid
-local total_area = 0
-for char in filled_grid:gmatch("#") do
-  total_area = total_area + 1
-end
-logd("Total distance covered: " .. total_distance)
-print("Area of polygon: " .. total_area)
+
+print("Part 1 answer: " .. calculate_area(vertices))
